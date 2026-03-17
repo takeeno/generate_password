4c1fcf1246c93cd10b6731721964225aac94863b
Takeshi Enomoto <81869777+takeeno@users.noreply.github.com>
2026-03-17 06:20:38 +0000

    return

--- pw_gen.py
+++ pw_gen.py
@@ -4,3 +4,4 @@ import string
 def generate_password(length=16):
   characters = string.ascii_letters + string.digits + string.punctuation
   password = ''.join(secrets.choice(characters) for _ in range(length))
+  return password
